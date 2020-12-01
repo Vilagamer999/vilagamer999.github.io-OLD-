be51a2fedead09a527cca8440232026a66a73eea
Vila <30276916+Vilagamer999@users.noreply.github.com>
2020-12-01 21:44:37 +0100

    Update title.js

--- assets/mobile/title.js
+++ assets/mobile/title.js
@@ -16,7 +16,7 @@ function loaded() {
 
 var x = 0;
 
-var titleText = ["v", "vi", "vil", "vila", "vila", "vila", "vil", "vi", "v"];
+var titleText = ["v", "vi", "vil", "vila", "vila", "vila", "vil", "vi", "v", " "];
 
 function loop() {
 
